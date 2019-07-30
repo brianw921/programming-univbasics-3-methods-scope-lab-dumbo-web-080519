e406db80e9f222d6692135cc8444c3fda5190ab2
Brian Wong <bwong92@gmail.com>
2019-07-30 00:56:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -12,8 +12,8 @@ def link
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
-
-def any_phrase(phrase)
   phrase = "Do A Barrel Roll!"
+def any_phrase(phrase)
+  
   puts phrase
 end
